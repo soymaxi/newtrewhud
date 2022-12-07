3e7a3c771c833f703bd3417de311fd8c054c599b
Maxi <63165345+soymaxi@users.noreply.github.com>
2022-12-07 15:42:04 -0300

    newui
    Añadidos los archivos :+1:

---
+++ config.lua
@@ -0,0 +1,73 @@
+Config = {}
+
+Config.Locale = 'es'
+
+Config.serverLogo = 'https://i.imgur.com/AcgDL9f.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'K',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= false,
+
+	showHealth			= false,
+	showArmor	 		= false,
+	showStamina	 		= false,
+	showHunger 			= false,
+	showThirst	 		= false,
+
+	showMinimap			= true,
+
+	showWeapons			= false,	
+}
+
+-- REDISEÑO DE TREW HUD UI DE  BY Maxii#9472 --
+-- REDISEÑO DE TREW HUD UI DE  BY Maxii#9472 --
+-- REDISEÑO DE TREW HUD UI DE  BY Maxii#9472 --
+-- REDISEÑO DE TREW HUD UI DE  BY Maxii#9472 --
